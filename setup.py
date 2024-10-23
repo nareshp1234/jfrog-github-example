37f67fbdc585b10cd8071a5deb64fb9c5f4c976b
nagag08 <110815496+nagag08@users.noreply.github.com>
2024-10-23 17:31:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     author_email='jfrog@jfrog.com',
     url='https://github.com/carmithersh/carmit-testing',
     packages=['pythonExample'],
-    install_requires=['PyYAML>3.11', 'nltk'],
+    install_requires=['PyYAML==3.11', 'nltk','paramiko==1.17.5', 'urllib3==1.25.2','redis == 4.6.0','Flask==2.3.2','gunicorn==22.0.0'],
 )
